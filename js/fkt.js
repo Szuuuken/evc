64268f243e47ccb1126c8b7b5e24c0098e59d516
Daniel Szukitsch <daniel@szuuuken.at>
2014-04-07 14:11:02 +0200

    v0.0.0.0.1

--- js/fkt.js
+++ js/fkt.js
@@ -1,15 +1,60 @@
-asked_quenstions = new Array();
-
-not_asked_qeustions = [
-	{question : "Die sphärische Aberration führt zu Farbverfälschungen im Bild.", correct: false},
-	{question : "Je größer die Brennweite, desto kleiner erscheinen Objekte im Bild", correct: true}
-];
-
 function get_random_quenstion(){
-	question_id = Math.floor(Math.random() * not_asked_qeustions.length)
-	return not_asked_qeustions[question_id]
+	question_id = 0;
+	if(not_asked_qeustions.length >= 1){
+		question_id = Math.floor((Math.random() * not_asked_qeustions.length ) % not_asked_qeustions.length)
+	}
+
+	console.log(not_asked_qeustions.length);
+
+	current_question = not_asked_qeustions[question_id];
+	asked_quenstions.push(current_question);
+
+	if(question_id == 0){
+		not_asked_qeustions.splice(0,1);	
+	}else{
+		not_asked_qeustions.splice(question_id,question_id);
+	}
+	
+	return current_question;
 }
 
 function btn_go() {
    window.location = "fragen.html";
 }
+
+function show_next_question(){
+	$('#answer_box').removeClass('correct')
+	$('#answer_box').removeClass('incorrect')
+	$('#btn_wrong').removeClass('hidden')
+	$('#btn_right').removeClass('hidden')
+	$('#next_question_box').addClass('hidden');
+	show_random_question();
+}
+
+function show_random_question(){
+	if(not_asked_qeustions.length > 0){
+		question = get_random_quenstion();
+		console.log(question);
+		$("#question_box").text(question.question)
+	}else{
+		alert('keine Fragen mehr');
+	}
+}
+
+function awnser_button(button){
+	if((button == "btn_right" && current_question.correct == true) || (button == "btn_wrong" && current_question.correct == false)){
+		$('#answer_box').addClass('correct')
+	}else{
+		$('#answer_box').addClass('incorrect')
+	}
+
+	if(current_question.correct == true){
+		$('#btn_wrong').addClass('hidden');
+	}else{
+		$('#btn_right').addClass('hidden');
+	}
+
+	$('#next_question_box').removeClass('hidden');
+}
+
+$(window).load(show_random_question);
